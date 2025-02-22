64eb89b05d0d4beaa47187a1664a9f62cd07cb1b
abdelrahman830a <abdelrahman.1220005@stemalex.moe.edu.eg>
2025-02-22 14:54:47 +0200

    Loading spinner

--- components/AnchorFinder.tsx
+++ components/AnchorFinder.tsx
@@ -128,6 +128,29 @@ const AnchorFinder = () => {
         </button>
       </form>
       {error && <p className="text-red-500 mt-4 text-center">{error}</p>}
+
+      {loading && (
+        <div className="mt-10 flex items-center justify-center">
+          <svg
+            className="animate-spin h-24 w-24 text-blue-600"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24">
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8v8H4z"></path>
+          </svg>
+        </div>
+      )}
+
       {result && (
         <div className="mt-6 space-y-6">
           {renderAnchorTexts()}
